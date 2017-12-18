80787e621a0fb54aa0ee485da9ff210b9f6011eb
Jeremy Gruhlkey <jeremygruhlkey@Naeuns-MacBook-Pro.local>
2017-12-18 15:38:29 -0800

    Basic HTML written
    css started
    some js psuedo code

---
+++ assets/javascript/app.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    
+    var questions = [
+        globe = {
+            question: "The earth is a spinning globe.",
+            correct: "false"
+        },
+
+        gravity = {
+            question: "Gravity holds things to the earth.",
+            correct: "false"
+        }
+        
+    ]
+
+    $(".choice").click(function() { 
+        
+
+    });
+});
