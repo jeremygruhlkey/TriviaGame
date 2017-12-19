d300d0cd11562adab89069c982d81ca21f7fdb7a
Jeremy Gruhlkey <jeremygruhlkey@Naeuns-MacBook-Pro.local>
2017-12-18 18:48:47 -0800

    Start button works, prints question to html from array
    that's it so far

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,40 @@
 $(document).ready(function(){
     
-    var questions = [
+    var questionSet = [
         globe = {
             question: "The earth is a spinning globe.",
-            correct: "false"
+            correct: "false",
+            rightText: "Correct! The Earth as globe is myth! It's actually flat!",
+            wrongText: "Wrong, you sheep! It's flat! NASA is lying to you!"
         },
 
         gravity = {
             question: "Gravity holds things to the earth.",
-            correct: "false"
+            correct: "false",
+            rightText: "Correct! The flat Earth accelerates upward forcing you into it!",
+            wrongText: "WRONG!! Gravity is a lie. Earth accelerates upward forcing you into it."
         }
         
-    ]
+    ];
 
-    $(".choice").click(function() { 
-        
+    var wrongAnswers = 0;
+    var rightAnswers = 0;
+    var arrayPosition = 0;
+    var currentQuestion;
+
+    $(".start").click(function() { 
+        $(".start").addClass("hide");
+        $(".game-box").addClass("unhide");
+        makeGuess();
 
     });
+
+    function makeGuess(){
+        currentQuestion = questionSet[arrayPosition].question;
+        console.log(currentQuestion)
+        $(".questions").html(currentQuestion);
+        $(".option").click(function() {
+
+        });
+    };
 });
